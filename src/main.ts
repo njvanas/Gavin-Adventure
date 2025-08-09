99c7fa067a13928ec92fe228c2ce1a31138ed654
Barend Du Preez <96685991+Barend183@users.noreply.github.com>
2025-08-09 19:00:24 +0200

    feat: add extended level 1 with helpers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { k } from "./game";
 import level1 from "./scenes/level1";
+import level1_long from "./scenes/level1_long";
 
 k.scene("level1", level1);
+k.scene("level1_long", level1_long);
 k.scene("level2", () => {
   k.add([k.text("Level 2 (WIP)", { size: 24 }), k.pos(120, 100)]);
 });
 
-k.go("level1");
+// Boot into the long level by default for now
+k.go("level1_long");
